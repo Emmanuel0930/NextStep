ae0e2e8db9173c26aade1efaede85b28512d1dad
Johan-Rico <jsricon@eafit.edu.co>
2025-11-15 20:48:24 -0500

    Corrección error con la URL del backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@ const config = {
   // URL del backend API
   // Para desarrollo local: http://localhost:5000/api
   // Para túnel:    https://nextstep-back.loca.lt/api
-  API_URL: 'http://localhost:5000',
+  API_URL: 'http://localhost:5000/api',
 };
 
 export default config;
